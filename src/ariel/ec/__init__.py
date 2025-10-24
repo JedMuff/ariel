6c39d01e72563f3388458eb52d82eb4b8d223bda
JedMuff <jed.muff@mac.com>
2025-10-24 14:01:36 +0200

    Adding tree genotype

--- src/ariel/ec/__init__.py
+++ src/ariel/ec/__init__.py
@@ -1,1 +1,17 @@
 """ARIEL EA Framework package."""
+
+from . import evaluation, selection, strategies
+from .a001 import Individual
+from .genotypes import Genotype, NeuralDevelopmentalEncoding, TreeGenotype
+from .strategies import MuLambdaStrategy
+
+__all__ = [
+    "Individual",
+    "Genotype",
+    "TreeGenotype",
+    "NeuralDevelopmentalEncoding",
+    "MuLambdaStrategy",
+    "evaluation",
+    "selection",
+    "strategies",
+]
--- src/ariel/ec/genotypes/__init__.py
+++ src/ariel/ec/genotypes/__init__.py
@@ -1,1 +1,7 @@
 """Genotypes and their operators."""
+
+from .base import Genotype
+from .nde import NeuralDevelopmentalEncoding
+from .tree import TreeGenotype
+
+__all__ = ["Genotype", "NeuralDevelopmentalEncoding", "TreeGenotype"]
