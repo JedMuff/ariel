5af7b8407fb67a9c4cb17581eedfe70e11556921
JedMuff <jed.muff@mac.com>
2025-10-24 14:00:29 +0200

    Dividing decoder functions to be more structured

--- src/ariel/body_phenotypes/robogen_lite/decoders/hi_prob_decoding.py
+++ src/ariel/body_phenotypes/robogen_lite/decoders/hi_prob_decoding.py
@@ -12,25 +12,19 @@ Todo
 ----
 - [ ] for loops to be replaced with vectorized operations
 - [ ] DiGraph positioning use cartesian coordinates instead of spring layout
-- [ ] Should probably move the graph functions to a separate script
 """
 
 # Evaluate type annotations in a deferred manner (ruff: UP037)
 from __future__ import annotations
 
-import json
-
 # Standard library
-from pathlib import Path
 from typing import Any
 
 # Third-party libraries
-import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import numpy.typing as npt
 from networkx import DiGraph
-from networkx.readwrite import json_graph
 
 # Local libraries
 from ariel import log
@@ -47,7 +41,6 @@ from ariel.body_phenotypes.robogen_lite.config import (
 
 # Global constants
 SEED = 42
-DPI = 300
 
 # Global functions
 RNG = np.random.default_rng(SEED)
@@ -276,107 +269,3 @@ class HighProbabilityDecoder:
 
         # Core is always a parent, never a child
         self.conn_p_space[:, IDX_OF_CORE, :] = 0.0
-
-
-def save_graph_as_json(
-    graph: DiGraph[Any],
-    save_file: Path | str | None = None,
-) -> None:
-    """
-    Save a directed graph as a JSON file.
-
-    Parameters
-    ----------
-    graph : DiGraph
-        The directed graph to save.
-    save_file : Path | str | None, optional
-        The file path to save the graph JSON, by default None
-    """
-    if save_file is None:
-        return
-
-    data = json_graph.node_link_data(graph, edges="edges")
-    json_string = json.dumps(data, indent=4)
-
-    with Path(save_file).open("w", encoding="utf-8") as f:
-        f.write(json_string)
-
-
-def load_graph_from_json(
-    load_file: Path | str,
-) -> DiGraph[Any]:
-    """
-    Load a directed graph from a JSON file.
-
-    Parameters
-    ----------
-    load_file : Path | str
-        The file path to load the graph JSON.
-
-    Returns
-    -------
-    DiGraph
-        The loaded directed graph.
-    """
-    with Path(load_file).open("r", encoding="utf-8") as f:
-        data = json.load(f)
-    return json_graph.node_link_graph(
-        data,
-        directed=True,
-        multigraph=False,
-        edges="edges",
-    )
-
-
-def draw_graph(
-    graph: DiGraph[Any],
-    title: str = "NetworkX Directed Graph",
-    save_file: Path | str | None = None,
-) -> None:
-    """
-    Draw a directed graph.
-
-    Parameters
-    ----------
-    graph : DiGraph
-        The directed graph to draw.
-    title : str
-        The title of the graph.
-    save_file : Path | str | None, optional
-        The file path to save the graph image, by default None
-    """
-    plt.figure()
-
-    pos = nx.spectral_layout(graph)
-
-    pos = nx.spring_layout(graph, pos=pos, k=1, iterations=20, seed=SEED)
-
-    nx.draw(
-        graph,
-        pos,
-        with_labels=True,
-        node_size=150,
-        node_color="#FFFFFF00",
-        edgecolors="blue",
-        font_size=8,
-        width=0.5,
-    )
-
-    edge_labels = nx.get_edge_attributes(graph, "face")
-
-    nx.draw_networkx_edge_labels(
-        graph,
-        pos,
-        edge_labels=edge_labels,
-        font_color="red",
-        font_size=8,
-    )
-
-    plt.title(title)
-
-    # Save the graph visualization
-    if save_file:
-        plt.savefig(save_file, dpi=DPI)
-    else:
-        # Show the plot
-        plt.show()
